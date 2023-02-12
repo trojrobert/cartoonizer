256982968d0f17f7da8f178d5a5b16a78e04b22d
robert <trojrobert@gmail.com>
2023-02-12 07:19:41 +0100

    add frontend

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
+
+import UploadImages from "./components/image-upload.component";
+
+function App() {
+  return (
+    <div className="container">
+      <h3>Cartoonizer App</h3>
+      <h4>Upload your image</h4>
+
+      <div className="content">
+        <UploadImages />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>
+//           Hello world, I am John Robert, I am getting started with React. 
+//         </p> 
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
