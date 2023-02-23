27be22896c24321857eb3dfa0e2a87d0c6f874a2
Michael Eze <michaeleze3@gmail.com>
2023-02-23 02:26:30 +0000

    revert service

--- client-next/src/services/upload.service.ts
+++ client-next/src/services/upload.service.ts
@@ -1,11 +1,12 @@
 class UploadService {
   static async upload(file: Blob | undefined) {
+    const formData = new FormData();
+
     try {
       const response = await fetch("http://0.0.0.0:8080/decolorize", {
         method: "POST",
-        body: JSON.stringify({file})
+        body: formData.append("file", file, file.name)
       });
-
       const data = await response.json();
 
       return Promise.resolve(data);
