45ef0988e64fbdf332f69bd12f77c83bdde56ad6
robert <trojrobert@gmail.com>
2023-03-13 21:27:48 +0100

    add docker-compose.yml

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
+
+import UploadImages from "./components/image-upload.component";
+
+function App() {
+  return (
+    <div className="container">
+      <h3>Decolourizer AI</h3>
+
+      <div className="content">
+        <UploadImages />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>
+//           Hello world, I am John Robert, I am getting started with React. 
+//         </p> 
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
